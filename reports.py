72bc4cbaa5e741e23b665d112c6e35e7ac86e074
Robert Kohanyi <robert.kohanyi@codecool.com>
2016-11-29 15:09:39 +0000

    Add count_games implementation

--- reports.py
+++ reports.py
@@ -1,2 +1,11 @@
+def get_table(file_name):
+    table = []
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            parts = line.split('\t')
+            table.append(parts)
+    return table
 
-# Report functions
+
+def count_games(file_name):
+    return len(get_table(file_name))
