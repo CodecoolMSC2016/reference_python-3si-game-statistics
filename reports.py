71dcfd4fe7dc450ce63b0afaeabb23c7228c7908
Robert Kohanyi <robert.kohanyi@codecool.com>
2016-11-29 15:10:31 +0000

    Add get_latest implementation

--- reports.py
+++ reports.py
@@ -17,3 +17,15 @@ def decide(file_name, year):
         if year == game_year:
             return True
     return False
+
+def get_latest(file_name):
+    max_title = None
+    max_year = None
+    table = get_table(file_name)
+    for row in table:
+        game_title = row[0]
+        game_year = int(row[2])
+        if max_year is None or max_year < game_year:
+            max_title = game_title
+            max_year = game_year
+    return max_title
