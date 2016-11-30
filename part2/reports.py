5598e6e96944e2a4c53e62af018c8d352ca2f668
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-11-30 09:15:26 +0000

    Add get_most_played implementation

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,16 @@
-
-# Report functions
+def get_most_played(file_name):
+    table = []
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            parts = line.split('\t')
+            table.append(parts)
+    most_played_title = None
+    max_units_sold = None
+    for row in table:
+        game_title = row[0]
+        units_sold = float(row[1])
+        if max_units_sold is None or max_units_sold < units_sold:
+            max_units_sold = units_sold
+            most_played_title = game_title
+    return most_played_title
