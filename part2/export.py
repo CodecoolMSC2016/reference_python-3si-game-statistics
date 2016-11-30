e8450f8ba0a953f4f6de8a6f9b315806d614e661
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-11-30 11:04:30 +0000

    Add export and printing functions
    Based on part1's export and printing functions.

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,84 @@
+import reports
 
-# Export functions
+
+def get_most_played(input_file):
+    res = reports.get_most_played(input_file)
+    print('What is the title of the most played game?', res, '\n')
+    return res
+
+
+def sum_sold(input_file):
+    res = str(reports.sum_sold(input_file))
+    print('How many copies have been sold total?', res, '\n')
+    return res
+
+
+def get_selling_avg(input_file):
+    res = str(reports.get_selling_avg(input_file))
+    print('What is the average selling?', res, '\n')
+    return res
+
+
+def count_longest_title(input_file):
+    res = str(reports.count_longest_title(input_file))
+    print('How many characters long is the longest title?', res, '\n')
+    return res
+
+
+def get_date_avg(input_file):
+    res = reports.get_date_avg(input_file)
+    print('What is the average of the release dates?', res, '\n')
+    return res
+
+
+def get_game(input_file):
+    title = input('Enter a title:')
+    res = reports.get_game(input_file, title)
+    res = [str(x) for x in res]
+    res = ', '.join(res)
+    print('What properties has a game?', res, '\n')
+    return res
+
+
+def count_grouped_by_genre(input_file):
+    res = reports.count_grouped_by_genre(input_file)
+    res = [k + ':' + str(v) for k, v in res.items()]
+    res = ', '.join(res)
+    print('How many games are there grouped by genre?', res, '\n')
+    return res
+
+
+def get_date_ordered(input_file):
+    res = reports.get_date_ordered(input_file)
+    res = ', '.join(res)
+    print('What is the date ordered list of the games? ', res, '\n')
+    return res
+
+
+def main():
+    input_file = "game_stat.txt"
+    with open('game_stat_export.txt', 'w') as output_file:
+        output_file.write(get_most_played(input_file))
+        output_file.write('\n')
+
+        output_file.write(sum_sold(input_file))
+        output_file.write('\n')
+
+        output_file.write(get_selling_avg(input_file))
+        output_file.write('\n')
+
+        output_file.write(count_longest_title(input_file))
+        output_file.write('\n')
+
+        output_file.write(get_game(input_file))
+        output_file.write('\n')
+
+        output_file.write(count_grouped_by_genre(input_file))
+        output_file.write('\n')
+
+        output_file.write(get_date_ordered(input_file))
+        output_file.write('\n')
+
+
+if __name__ == '__main__':
+    main()
--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,62 @@
+import reports
 
-# Printing functions
+
+def get_most_played(input_file):
+    res = reports.get_most_played(input_file)
+    print('What is the title of the most played game?', res, '\n')
+
+
+def sum_sold(input_file):
+    res = reports.sum_sold(input_file)
+    print('How many copies have been sold total?', res, '\n')
+
+
+def get_selling_avg(input_file):
+    res = reports.get_selling_avg(input_file)
+    print('What is the average selling?', res, '\n')
+
+
+def count_longest_title(input_file):
+    res = reports.count_longest_title(input_file)
+    print('How many characters long is the longest title?', res, '\n')
+
+
+def get_date_avg(input_file):
+    res = reports.get_date_avg(input_file)
+    print('What is the average of the release dates?', res, '\n')
+
+
+def get_game(input_file):
+    title = input('Enter a title:')
+    res = reports.get_game(input_file, title)
+    res = [str(x) for x in res]
+    res = ', '.join(res)
+    print('What properties has a game?', res, '\n')
+
+
+def count_grouped_by_genre(input_file):
+    res = reports.count_grouped_by_genre(input_file)
+    res = [k + ':' + str(v) for k, v in res.items()]
+    res = ', '.join(res)
+    print('How many games are there grouped by genre?', res, '\n')
+
+
+def get_date_ordered(input_file):
+    res = reports.get_date_ordered(input_file)
+    res = ', '.join(res)
+    print('What is the date ordered list of the games? ', res, '\n')
+
+
+def main():
+    input_file = "game_stat.txt"
+    get_most_played(input_file)
+    sum_sold(input_file)
+    get_selling_avg(input_file)
+    count_longest_title(input_file)
+    get_game(input_file)
+    count_grouped_by_genre(input_file)
+    get_date_ordered(input_file)
+
+
+if __name__ == '__main__':
+    main()
