c1b3fd2e433016997dac4106edecf0a0a4c3139a
Robert Kohanyi <robert.kohanyi@codecool.com>
2016-11-30 05:53:31 +0000

    Add some newlines to conform to PEP8

--- export.py
+++ export.py
@@ -1,9 +1,11 @@
 import reports
 
+
 def count_games(input_file):
     res = str(reports.count_games(input_file))
     print('How many games are in the file?', res, '\n')
     return res
+
 
 def decide(input_file):
     year = int(input('Enter a year:'))
@@ -11,10 +13,12 @@ def decide(input_file):
     print('Is there a game from a given year?', res, '\n')
     return res
 
+
 def get_latest(input_file):
     res = reports.get_latest(input_file)
     print('Which was the latest game?', res, '\n')
     return res
+
 
 def count_by_genre(input_file):
     genre = input('Enter a genre:')
@@ -22,11 +26,13 @@ def count_by_genre(input_file):
     print('How many games do we have by genre?', res, '\n')
     return res
 
+
 def get_line_number_by_title(input_file):
     title = input('Enter a title:')
     res = str(reports.get_line_number_by_title(input_file, title))
     print('What is the line number of the given game (by title)?', res, '\n')
     return res
+
 
 def sort_abc(input_file):
     res = reports.sort_abc(input_file)
@@ -34,16 +40,19 @@ def sort_abc(input_file):
     print('What is the alphabetical ordered list of the titles?', res, '\n')
     return res
 
+
 def get_genres(input_file):
     res = reports.get_genres(input_file)
     res = ', '.join(res)
     print('What are the genres?', res, '\n')
     return res
 
+
 def when_was_top_sold_fps(input_file):
     res = str(reports.when_was_top_sold_fps(input_file))
     print('What is the release date of the top sold "First-person shooter" game?', res, '\n')
     return res
+
 
 def main():
     input_file = "game_stat.txt"
@@ -68,6 +77,7 @@ def main():
 
         output_file.write(when_was_top_sold_fps(input_file))
         output_file.write('\n')
+
 
 if __name__ == '__main__':
     main()
--- printing.py
+++ printing.py
@@ -1,41 +1,50 @@
 import reports
 
+
 def count_games(input_file):
     res = reports.count_games(input_file)
     print('How many games are in the file?', res, '\n')
+
 
 def decide(input_file):
     year = int(input('Enter a year:'))
     res = reports.decide(input_file, year)
     print('Is there a game from a given year?', res, '\n')
 
+
 def get_latest(input_file):
     res = reports.get_latest(input_file)
     print('Which was the latest game?', res, '\n')
+
 
 def count_by_genre(input_file):
     genre = input('Enter a genre:')
     res = reports.count_by_genre(input_file, genre)
     print('How many games do we have by genre?', res, '\n')
 
+
 def get_line_number_by_title(input_file):
     title = input('Enter a title:')
     res = reports.get_line_number_by_title(input_file, title)
     print('What is the line number of the given game (by title)?', res, '\n')
+
 
 def sort_abc(input_file):
     res = reports.sort_abc(input_file)
     res = ', '.join(res)
     print('What is the alphabetical ordered list of the titles?', res, '\n')
 
+
 def get_genres(input_file):
     res = reports.get_genres(input_file)
     res = ', '.join(res)
     print('What are the genres?', res, '\n')
 
+
 def when_was_top_sold_fps(input_file):
     res = reports.when_was_top_sold_fps(input_file)
     print('What is the release date of the top sold "First-person shooter" game?', res, '\n')
+
 
 def main():
     input_file = "game_stat.txt"
@@ -46,6 +55,7 @@ def main():
     sort_abc(input_file)
     get_genres(input_file)
     when_was_top_sold_fps(input_file)
+
 
 if __name__ == '__main__':
     main()
