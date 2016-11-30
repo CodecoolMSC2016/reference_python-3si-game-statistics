774322b9184a6a07a55f543b54b48b6308e8eda7
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-11-30 09:19:10 +0000

    Add sum_sold implementation

--- part2/reports.py
+++ part2/reports.py
@@ -1,12 +1,17 @@
-def get_most_played(file_name):
+def get_table(file_name):
     table = []
     with open(file_name, 'r') as f:
         lines = f.readlines()
         for line in lines:
             parts = line.split('\t')
             table.append(parts)
+    return table
+
+
+def get_most_played(file_name):
     most_played_title = None
     max_units_sold = None
+    table = get_table(file_name)
     for row in table:
         game_title = row[0]
         units_sold = float(row[1])
@@ -14,3 +19,12 @@ def get_most_played(file_name):
             max_units_sold = units_sold
             most_played_title = game_title
     return most_played_title
+
+
+def sum_sold(file_name):
+    table = get_table(file_name)
+    sum_units_sold = 0
+    for row in table:
+        units_sold = float(row[1])
+        sum_units_sold += units_sold
+    return sum_units_sold
